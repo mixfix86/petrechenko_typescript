40c7cd740668ea628042fd8964baa58670fa8971
mix <mixfix@gmail.com>
2023-04-06 21:56:01 +0500

    view video

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ const userName: string = 'Max';
 const age: number = 35;
 const isBirthday: boolean = true;
 
+const number = 5 + 5;
+
 if (isBirthday) {
   console.log(`Поздравляю ${userName}, возраст ${age + 1}`);
 }
+
